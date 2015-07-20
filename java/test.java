9e3889d160f61333343c60ff48e764a8a3c71fd7
Rachel Kobayashi <r5ko@users.noreply.github.com>
2015-07-19 21:44:12 -0700

    add java makefile / test

---
+++ java/test.java
@@ -0,0 +1,19 @@
+/**
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.lang.Character;
+
+class TestApp {
+    private static void test(){
+	System.out.println("Hello World");
+    }
+
+    public static void main(String[] args) {
+	test();
+    }
+}
